758a2dbc69f926e13f88b69ed49a8dfa583abeb8
Mateusz Kozik <mateuszkozik100@gmail.com>
2019-10-21 12:31:38 +0200

    Init new repository

---
+++ src/test/java/com/DTeam/eshop/EshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DTeam.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
