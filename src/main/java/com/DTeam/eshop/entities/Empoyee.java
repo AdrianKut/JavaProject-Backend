a16835ab84084db43a49b595c7b39e1eb7e27521
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-02-26 09:06:32 +0100

    Add employee entity

---
+++ src/main/java/com/DTeam/eshop/entities/Empoyee.java
@@ -0,0 +1,56 @@
+package com.DTeam.eshop.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "employees")
+@Data
+public class Empoyee {
+
+    @Id    
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "employee_id", nullable = false)
+    private Long employeeId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+    @Column(name = "employment_date", nullable = false)
+    private LocalDate employmentDate;
+
+    @Column(name = "base_pay", nullable = false)
+    private BigDecimal basePay;
+
+    @Column(name = "extra_pay", nullable = false)
+    private BigDecimal extraPay;
+
+    @Column(name = "position", nullable = false)
+    private String position;
+
+    public String getEmployemntDate() {
+        if (employmentDate!= null) {
+           return employmentDate.toString();
+       } else {
+           return "";
+       }
+   }
+
+   public void setEmploymentDate(String employmentDate) {
+       LocalDate date = LocalDate.parse(employmentDate, DateTimeFormatter.ISO_LOCAL_DATE);
+       this.employmentDate = date;
+   }
+}
