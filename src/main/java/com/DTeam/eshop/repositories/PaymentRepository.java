a24bcf9fb736b537631097a0dc64087be86d8f27
MichalStygar <misiek9753@o2.pl>
2020-02-26 11:50:17 +0100

    Create payment repository

---
+++ src/main/java/com/DTeam/eshop/repositories/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.DTeam.eshop.repositories;
+
+import com.DTeam.eshop.entities.Payment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    
+}
