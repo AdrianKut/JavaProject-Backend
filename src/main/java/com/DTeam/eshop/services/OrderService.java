d6f554a709fcf5e8cfa367e85737fe439fc987ce
MichalStygar <misiek9753@o2.pl>
2020-02-26 11:57:41 +0100

    Create order service

---
+++ src/main/java/com/DTeam/eshop/services/OrderService.java
@@ -0,0 +1,31 @@
+package com.DTeam.eshop.services;
+
+import java.util.List;
+
+import com.DTeam.eshop.entities.Order;
+import com.DTeam.eshop.repositories.OrderRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    @Autowired private OrderRepository orderRepository;
+    
+    public List<Order> listAll(){
+        return orderRepository.findAll();
+    }
+
+    public void save(Order order){
+        orderRepository.save(order);
+    }
+
+    public Order get(Long id){
+        return orderRepository.findById(id).get();
+    }
+
+    public void delete(Long id){
+        orderRepository.deleteById(id);
+    }
+}
