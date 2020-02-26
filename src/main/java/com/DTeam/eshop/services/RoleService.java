b4c6a750f26301f1f664b9791f81bcbdf81c7ae0
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-02-26 12:56:46 +0100

    Create role service

---
+++ src/main/java/com/DTeam/eshop/services/RoleService.java
@@ -0,0 +1,31 @@
+package com.DTeam.eshop.services;
+
+import java.util.List;
+
+import com.DTeam.eshop.entities.Role;
+import com.DTeam.eshop.repositories.RoleRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService {
+
+    @Autowired private RoleRepository roleRepository;
+    
+    public List<Role> listAll(){
+        return roleRepository.findAll();
+    }
+
+    public void save(Role role){
+        roleRepository.save(role);
+    }
+
+    public void get(String name){
+        roleRepository.findById(name).get();
+    }
+
+    public void delete(String name){
+        roleRepository.deleteById(name);
+    }
+}
