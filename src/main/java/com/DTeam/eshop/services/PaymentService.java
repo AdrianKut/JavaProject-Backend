f657e0b46a5901fb7958e155adec69b25f59cb02
MichalStygar <misiek9753@o2.pl>
2020-02-26 11:59:55 +0100

    Create payment service

---
+++ src/main/java/com/DTeam/eshop/services/PaymentService.java
@@ -0,0 +1,31 @@
+package com.DTeam.eshop.services;
+
+import java.util.List;
+
+import com.DTeam.eshop.entities.Payment;
+import com.DTeam.eshop.repositories.PaymentRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    @Autowired private PaymentRepository paymentRepository;
+    
+    public List<Payment> listAll(){
+        return paymentRepository.findAll();
+    }
+
+    public void save(Payment payment){
+        paymentRepository.save(payment);
+    }
+
+    public Payment get(Long id){
+        return paymentRepository.findById(id).get();
+    }
+
+    public void delete(Long id){
+        paymentRepository.deleteById(id);
+    }
+}
