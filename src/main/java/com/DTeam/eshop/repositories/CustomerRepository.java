a0e4a7a59f96f53a3af2d23a4e634d64c2295d4e
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-02-26 12:41:10 +0100

    Create customer repository

---
+++ src/main/java/com/DTeam/eshop/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.DTeam.eshop.repositories;
+
+import com.DTeam.eshop.entities.Customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
