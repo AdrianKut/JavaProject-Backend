13bdbecf18be786b88ae415ef35f034c5fe01258
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-02-26 13:00:02 +0100

    Create employee service

---
+++ src/main/java/com/DTeam/eshop/services/EmployeeService.java
@@ -0,0 +1,31 @@
+package com.DTeam.eshop.services;
+
+import java.util.List;
+
+import com.DTeam.eshop.entities.Employee;
+import com.DTeam.eshop.repositories.EmployeeRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+    @Autowired private EmployeeRepository employeeRepository;
+
+    public List<Employee> listAll(){
+        return employeeRepository.findAll();
+    }
+
+    public void save(Employee employee){
+        employeeRepository.save(employee);
+    }
+
+    public Employee get(Long id){
+        return employeeRepository.findById(id).get();
+    }
+
+    public void delete(Long id){
+        employeeRepository.deleteById(id);
+    }
+}
