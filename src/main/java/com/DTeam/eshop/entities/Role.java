cbe7f192e97c06906e97be15244d3a831d924608
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-02-26 08:40:12 +0100

    Add role entity

---
+++ src/main/java/com/DTeam/eshop/entities/Role.java
@@ -0,0 +1,20 @@
+package com.DTeam.eshop.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+
+    @Id
+    @Column(name = "roles", nullable = false)
+    private String name;
+
+    public Role(){}
+}
