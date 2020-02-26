f7e5a41d20fee49c63bad44dd6250b784597c74d
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-02-26 13:12:59 +0100

    Create customer service

---
+++ src/main/java/com/DTeam/eshop/services/CustomerService.java
@@ -0,0 +1,31 @@
+package com.DTeam.eshop.services;
+
+import java.util.List;
+
+import com.DTeam.eshop.entities.Customer;
+import com.DTeam.eshop.repositories.CustomerRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    @Autowired private CustomerRepository customerRepository;
+
+    public List<Customer> listAll(){
+        return customerRepository.findAll();
+    }
+
+    public void save(Customer customer){
+        customerRepository.save(customer);
+    }
+
+    public Customer get(Long id){
+        return customerRepository.findById(id).get();
+    }
+
+    public void delete(Long id){
+        customerRepository.deleteById(id);
+    }
+}
