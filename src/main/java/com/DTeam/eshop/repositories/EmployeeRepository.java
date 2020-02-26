ee5712b82e2d0b8ccd4058bb980847c05cbdfba1
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-02-26 12:37:03 +0100

    Create employee repository

---
+++ src/main/java/com/DTeam/eshop/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.DTeam.eshop.repositories;
+
+import com.DTeam.eshop.entities.Employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+    
+}
