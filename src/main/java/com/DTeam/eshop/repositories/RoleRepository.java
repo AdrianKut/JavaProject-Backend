a93915203b24cdb94b83a1739b022132ca23a31b
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-02-26 12:36:00 +0100

    Create role repository

---
+++ src/main/java/com/DTeam/eshop/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.DTeam.eshop.repositories;
+
+import com.DTeam.eshop.entities.Role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, String> {
+
+}
