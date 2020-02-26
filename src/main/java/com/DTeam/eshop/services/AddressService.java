99c1d02cf78cf1e13335bf12a854ebae611217d0
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-02-26 13:09:12 +0100

    Create address service

---
+++ src/main/java/com/DTeam/eshop/services/AddressService.java
@@ -0,0 +1,31 @@
+package com.DTeam.eshop.services;
+
+import java.util.List;
+
+import com.DTeam.eshop.entities.Address;
+import com.DTeam.eshop.repositories.AddressRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressService {
+
+    @Autowired private AddressRepository addressRepository;
+
+    public List<Address> listAll(){
+        return addressRepository.findAll();
+    }
+
+    public void save(Address address){
+        addressRepository.save(address);
+    }
+
+    public Address get(Long id){
+        return addressRepository.findById(id).get();
+    }
+
+    public void delete(Long id){
+        addressRepository.deleteById(id);
+    }
+}
