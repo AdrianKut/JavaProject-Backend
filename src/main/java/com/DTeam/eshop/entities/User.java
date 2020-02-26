0404765c558218e4d19d21904e314bf710048847
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-02-26 08:45:01 +0100

    Add user entity

---
+++ src/main/java/com/DTeam/eshop/entities/User.java
@@ -0,0 +1,26 @@
+package com.DTeam.eshop.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "enabled", nullable = false)
+    private Boolean enabled = true;
+
+    public User(){}
+}
