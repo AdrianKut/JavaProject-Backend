8d9046a218532e9dbc93be94915ffdc9811855a3
MichalStygar <misiek9753@o2.pl>
2020-02-26 11:14:36 +0100

    Create @ManyToMany relation (products - orders)

--- src/main/java/com/DTeam/eshop/entities/Product.java
+++ src/main/java/com/DTeam/eshop/entities/Product.java
@@ -1,12 +1,14 @@
 package com.DTeam.eshop.entities;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -32,6 +34,9 @@ public class Product {
 
     @Column(name = "quantity", nullable = false)
     private Integer quantity;
+
+    @ManyToMany(mappedBy = "products")
+    private List<Order> orders;
 
     public Product(){}
 
