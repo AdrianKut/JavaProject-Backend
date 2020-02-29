f8cf7a7355338456d6ff1efaec34bd999e8e0b33
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-02-29 21:46:59 +0100

    Delete unused imports

--- src/main/java/com/DTeam/eshop/entities/Employee.java
+++ src/main/java/com/DTeam/eshop/entities/Employee.java
@@ -14,7 +14,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
 
 import lombok.Data;
