bb649233cd8428357a9a0ec54601fdf476eb0bb0
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-01 16:23:45 +0100

    Create customer - address relationship methods

--- src/main/java/com/DTeam/eshop/controllers/CustomerController.java
+++ src/main/java/com/DTeam/eshop/controllers/CustomerController.java
@@ -2,7 +2,9 @@ package com.DTeam.eshop.controllers;
 
 import java.util.List;
 
+import com.DTeam.eshop.entities.Address;
 import com.DTeam.eshop.entities.Customer;
+import com.DTeam.eshop.services.AddressService;
 import com.DTeam.eshop.services.CustomerService;
 import com.DTeam.eshop.utilities.CustomErrorType;
 
@@ -25,6 +27,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class CustomerController {
 
     @Autowired private CustomerService customerService;
+    @Autowired private AddressService addressService;
 
     //Retrieve all customers
     @GetMapping("/customers")
@@ -86,4 +89,82 @@ public class CustomerController {
         customerService.delete(customerId);
         return new ResponseEntity<Customer>(HttpStatus.NO_CONTENT);
     }
+
+      //Retrieve a address
+      @GetMapping("/customers/{id}/addresses")
+      public ResponseEntity<?> getAddress(@PathVariable("id")Long customerId){
+          if(!customerService.isCustomerExist(customerId)){
+              return new ResponseEntity<>(new CustomErrorType("Customer with id " + customerId + " not found."), HttpStatus.NOT_FOUND);
+          }
+          Customer customer = customerService.get(customerId);
+          if(customer.getAddress() == null){
+              return new ResponseEntity<>(new CustomErrorType("Customer with id " + customerId + " has no address assigned yet."), HttpStatus.NOT_FOUND); 
+          }
+          Address address = customer.getAddress();
+          return new ResponseEntity<Address>(address, HttpStatus.OK);
+      }
+  
+      //Create a address
+      @PostMapping("/customers/{id}/addresses")
+      public ResponseEntity<?> createAdress(@PathVariable("id")Long customerId, @RequestBody Address address, UriComponentsBuilder ucBuilder){
+          if(!customerService.isCustomerExist(customerId)){
+              return new ResponseEntity<>(new CustomErrorType("Unable to create. Customer with id " + customerId + " not found."),
+              HttpStatus.NOT_FOUND);
+          }
+          Customer customer = customerService.get(customerId);
+          if(customer.getAddress() != null){
+              return new ResponseEntity<>(new CustomErrorType("Unable to create. Customer with id " + customerId + " has already address."),
+              HttpStatus.CONFLICT);
+          }
+          addressService.save(address);
+          customer.setAddress(address);
+          customerService.save(customer);
+          HttpHeaders headers = new HttpHeaders();
+          headers.setLocation(ucBuilder.path("/api/addresses/{id}").buildAndExpand(address.getAddressId()).toUri());
+          return new ResponseEntity<String>(headers, HttpStatus.CREATED);
+      }
+      
+      //Update a address
+      @PutMapping("/customers/{customerid}/addresses/{addressid}")
+      public ResponseEntity<?> updateAdress(@PathVariable("customerid")Long customerId, 
+      @PathVariable("addressid")Long addressId, @RequestBody Address address){
+          if(!customerService.isCustomerExist(customerId)){
+              return new ResponseEntity<>(new CustomErrorType("Unable to update. Customer with id " + customerId + " not found."),
+              HttpStatus.NOT_FOUND);
+          }
+          if(!addressService.isAddressExist(addressId)){
+              return new ResponseEntity<>(new CustomErrorType("Unable to update. Address with id " + addressId + " not found."),
+              HttpStatus.NOT_FOUND); 
+          }
+          Address currentAddress = addressService.get(addressId);
+          currentAddress.setStreet(address.getStreet());
+          currentAddress.setHouseNumber(address.getHouseNumber());
+          currentAddress.setFlatNumber(address.getFlatNumber());
+          currentAddress.setPostcode(address.getPostcode());
+          currentAddress.setCity(address.getCity());
+          addressService.save(currentAddress);
+          return new ResponseEntity<Address>(currentAddress, HttpStatus.OK);
+      }
+      
+      //Create the association
+      @PostMapping("/customers/{customerid}/addresses/{addressid}")
+      public ResponseEntity<?> associateAdress(@PathVariable("customerid")Long customerId, 
+      @PathVariable("addressid")Long addressId){
+          if(!customerService.isCustomerExist(customerId)){
+              return new ResponseEntity<>(new CustomErrorType("Unable to associate. Customer with id " + customerId + " not found."),
+              HttpStatus.NOT_FOUND);
+          }
+          if(!addressService.isAddressExist(addressId)){
+              return new ResponseEntity<>(new CustomErrorType("Unable to associate. Address with id " + addressId + " not found."),
+              HttpStatus.NOT_FOUND); 
+          }
+          Customer customer = customerService.get(customerId);
+          if(customer.getAddress() != null){
+              return new ResponseEntity<>(new CustomErrorType("Unable to associate. Customer with id " + customerId + " has already address."),
+              HttpStatus.CONFLICT);
+          }
+          customer.setAddress(addressService.get(addressId));
+          customerService.save(customer);
+          return new ResponseEntity<Customer>(customer, HttpStatus.CREATED);
+      }
 }
