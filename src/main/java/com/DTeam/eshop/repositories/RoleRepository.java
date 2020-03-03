12adb6dd6d3b902748712346f73a391270ddb6b4
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-03 16:05:33 +0100

    Add get roles by user email method

--- src/main/java/com/DTeam/eshop/repositories/RoleRepository.java
+++ src/main/java/com/DTeam/eshop/repositories/RoleRepository.java
@@ -1,9 +1,11 @@
 package com.DTeam.eshop.repositories;
 
+import java.util.List;
+
 import com.DTeam.eshop.entities.Role;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, String> {
-
+    public List<Role> findByUsersEmail(String email);
 }
