618f46455c4fdbcc94488039747bf575d5c1570f
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-03 19:05:22 +0100

    Add get products by order id method

--- src/main/java/com/DTeam/eshop/repositories/ProductRepository.java
+++ src/main/java/com/DTeam/eshop/repositories/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.DTeam.eshop.repositories;
 
+import java.util.List;
+
 import com.DTeam.eshop.entities.Product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    
+    List<Product> findByOrdersOrderId(Long orderId);
 }
