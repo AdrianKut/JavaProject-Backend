0921a8e012c1228b91a962663b0923e8a0fad77a
MichalStygar <misiek9753@o2.pl>
2020-03-17 10:56:26 +0100

    Create payment controller

---
+++ src/main/java/com/DTeam/eshop/controllers/PaymentController.java
@@ -0,0 +1,72 @@
+package com.DTeam.eshop.controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import com.DTeam.eshop.entities.Payment;
+import com.DTeam.eshop.services.PaymentService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    @RequestMapping(value = "/payment/list", method = RequestMethod.GET)
+    public String getAll(Model model){
+        List<Payment> paymentList = paymentService.listAll();
+        model.addAttribute("paymentList",paymentList);
+        return "/views/payment/list";
+    }
+
+    @RequestMapping(value = "/payment/add", method = RequestMethod.GET)
+    public String save(Model model) {
+        Payment payment = new Payment();
+        model.addAttribute("payment", payment);
+        return "/views/payment/add";
+    }
+
+    @RequestMapping(value = "/payment/add", method = RequestMethod.POST)
+    public String save(@Valid Payment payment, BindingResult bindingResult){
+         if(bindingResult.hasErrors()){
+              return "views/payment/add"; 
+        }
+        paymentService.save(payment);
+        return "redirect:/payment/list";
+    }
+
+    @RequestMapping(value = "/payment/edit/{id}", method = RequestMethod.GET)
+    public String edit(Model model,@PathVariable(name = "id") long id){
+        
+        Payment payment = paymentService.get(id);
+        model.addAttribute("payment", payment);
+        return "/views/payment/edit";
+    }
+    
+    @RequestMapping(value = "/payment/edit/{id}", method = RequestMethod.POST)
+    public String edit(@PathVariable(name = "id") long id, 
+    @Valid Payment payment, BindingResult bindingResult){ 
+        if(bindingResult.hasErrors()){
+            return "views/payment/edit"; 
+      }
+      payment.setPaymentId(id);
+      paymentService.save(payment);
+        return "redirect:/payment/list";
+    }
+
+    @RequestMapping(value = "/payment/delete/{id}", method = RequestMethod.GET)
+    public String delete(@PathVariable(name = "id") long id){
+        paymentService.delete(id);
+        return "redirect:/payment/list";
+    }
+
+
+}
