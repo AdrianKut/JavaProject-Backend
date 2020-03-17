f1b3964c7f272c7f437e8320a6aeed093a14b7ea
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-17 13:03:24 +0100

    Add controller annotation

--- src/main/java/com/DTeam/eshop/controllers/RoleController.java
+++ src/main/java/com/DTeam/eshop/controllers/RoleController.java
@@ -6,11 +6,13 @@ import com.DTeam.eshop.entities.Role;
 import com.DTeam.eshop.services.RoleService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class RoleController {
 
     @Autowired private RoleService roleService;
