51c393c27e9b279d1ed27ab949084fce5510af38
MichalStygar <misiek9753@o2.pl>
2020-03-17 17:17:34 +0100

    Change order controller

--- src/main/java/com/DTeam/eshop/controllers/OrderController.java
+++ src/main/java/com/DTeam/eshop/controllers/OrderController.java
@@ -1,11 +1,12 @@
 package com.DTeam.eshop.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
-
-
 import com.DTeam.eshop.entities.Order;
+import com.DTeam.eshop.entities.Product;
 import com.DTeam.eshop.services.OrderService;
+import com.DTeam.eshop.services.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,11 +14,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class OrderController {
     @Autowired
     private OrderService orderService;
+
+    @Autowired
+    private ProductService productService;
 
     @RequestMapping(value = "/order/list", method = RequestMethod.GET)
     public String getAll(Model model){
@@ -28,32 +33,37 @@ public class OrderController {
 
     @RequestMapping(value = "/order/add", method = RequestMethod.GET)
     public String save(Model model) {
+        List<Product> productList = productService.listAll();
         Order order = new Order();
         model.addAttribute("order", order);
+        model.addAttribute("productList", productList);
         return "/views/order/add";
     }
 
     @RequestMapping(value = "/order/add", method = RequestMethod.POST)
-    public String save(Order order){
+    public String save(Order order,
+    @RequestParam(name="product",required = false)ArrayList<Product> product){
          
-        orderService.save(order);
+        orderService.save(order,product);
         return "redirect:/order/list";
     }
 
     @RequestMapping(value = "/order/edit/{id}", method = RequestMethod.GET)
     public String edit(Model model,@PathVariable(name = "id") long id){
-        
+        List<Product> productList = productService.listAll();
         Order order = orderService.get(id);
         model.addAttribute("order", order);
+        model.addAttribute("productList", productList);
         return "/views/order/edit";
     }
     
     @RequestMapping(value = "/order/edit/{id}", method = RequestMethod.POST)
     public String edit(@PathVariable(name = "id") long id, 
-    Order order){ 
+    Order order,
+    @RequestParam(name="product",required = false)ArrayList<Product> product){ 
        
         order.setOrderId(id);
-        orderService.save(order);
+        orderService.save(order,product);
         return "redirect:/order/list";
     }
 
