f3189624f3589aab7112daa5a4d75678a749fe8b
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-17 12:57:11 +0100

    Create role controller

---
+++ src/main/java/com/DTeam/eshop/controllers/RoleController.java
@@ -0,0 +1,43 @@
+package com.DTeam.eshop.controllers;
+
+import java.util.List;
+
+import com.DTeam.eshop.entities.Role;
+import com.DTeam.eshop.services.RoleService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class RoleController {
+
+    @Autowired private RoleService roleService;
+
+    @GetMapping("/role/list")
+    public String getAll(Model model){
+        List<Role> roleList = roleService.listAll();
+        model.addAttribute("roleList", roleList);
+        return "views/role/list";
+    }
+
+    @GetMapping("/role/add")
+    public String save(Model model){
+        Role role = new Role();
+        model.addAttribute("role", role);
+        return "views/role/add";
+    }
+
+    @PostMapping("/role/add")
+    public String save(Role role){
+        roleService.save(role);
+        return "redirect:/role/list";
+    }
+
+    @GetMapping(value = "role/delete/{name}")
+    public String delete(@PathVariable("name")String name){
+        roleService.delete(name);
+        return "redirect:/role/list";
+    }
+}
