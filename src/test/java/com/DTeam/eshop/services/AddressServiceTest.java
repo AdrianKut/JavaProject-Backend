9126e7364cbaea0ab258f00b730c79442e63e7d4
adrin96 <adrin99@o2.pl>
2020-03-17 20:49:29 +0100

    Create empty AddressServiceTest file

---
+++ src/test/java/com/DTeam/eshop/services/AddressServiceTest.java
@@ -0,0 +1,33 @@
+package com.DTeam.eshop.services;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AddressServiceTest {
+
+    public AddressServiceTest() {
+    }
+
+    @Test
+    public void testListAll() {
+    }
+
+    @Test
+    public void testSave() {
+    }
+
+    @Test
+    public void testGet() {
+    }
+
+    @Test
+    public void testDelete() {
+    }
+
+    @Test
+    public void testIsAddressExist() {
+    }
+
+}
