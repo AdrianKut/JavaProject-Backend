d956018ba60fd5dd18e9665b6274b0c45d2373e8
MichalStygar <misiek9753@o2.pl>
2020-03-18 10:28:20 +0100

    Add customer and address to order controller

--- src/main/java/com/DTeam/eshop/controllers/OrderController.java
+++ src/main/java/com/DTeam/eshop/controllers/OrderController.java
@@ -3,8 +3,12 @@ package com.DTeam.eshop.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.DTeam.eshop.entities.Address;
+import com.DTeam.eshop.entities.Customer;
 import com.DTeam.eshop.entities.Order;
 import com.DTeam.eshop.entities.Product;
+import com.DTeam.eshop.services.AddressService;
+import com.DTeam.eshop.services.CustomerService;
 import com.DTeam.eshop.services.OrderService;
 import com.DTeam.eshop.services.ProductService;
 
@@ -25,6 +29,12 @@ public class OrderController {
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private AddressService addressService;
+
     @GetMapping("/order/list")
     public String getAll(Model model){
         List<Order> orderList = orderService.listAll();
@@ -35,16 +45,24 @@ public class OrderController {
     @GetMapping("/order/add")
     public String save(Model model) {
         List<Product> productList = productService.listAll();
+        List<Customer> customerList = customerService.listAll();
+        List<Address> addressList = addressService.listAll();
         Order order = new Order();
         model.addAttribute("order", order);
         model.addAttribute("productList", productList);
+        model.addAttribute("customerList", customerList);
+        model.addAttribute("addressList", addressList);
         return "views/order/add";
     }
 
     @PostMapping("/order/add")
     public String save(Order order,
-    @RequestParam(name="product",required = false)ArrayList<Product> product){
+    @RequestParam(name="product",required = false)ArrayList<Product> product,
+    @RequestParam(name="customer")Customer customer,
+    @RequestParam(name="address")Address address){
 
+        order.setCustomer(customer);
+        order.setAddress(address);
         orderService.save(order,product);
         return "redirect:/order/list";
     }
@@ -52,18 +70,26 @@ public class OrderController {
     @GetMapping("/order/edit/{id}")
     public String edit(Model model,@PathVariable(name = "id") long id){
         List<Product> productList = productService.listAll();
+        List<Customer> customerList = customerService.listAll();
+        List<Address> addressList = addressService.listAll();
         Order order = orderService.get(id);
         model.addAttribute("order", order);
         model.addAttribute("productList", productList);
+        model.addAttribute("customerList", customerList);
+        model.addAttribute("addressList", addressList);
         return "views/order/edit";
     }
 
     @PostMapping("/order/edit/{id}")
     public String edit(@PathVariable(name = "id") long id,
     Order order,
-    @RequestParam(name="product",required = false)ArrayList<Product> product){
+    @RequestParam(name="product",required = false)ArrayList<Product> product,
+    @RequestParam(name="customer")Customer customer,
+    @RequestParam(name="address")Address address){
 
         order.setOrderId(id);
+        order.setCustomer(customer);
+        order.setAddress(address);
         orderService.save(order,product);
         return "redirect:/order/list";
     }
