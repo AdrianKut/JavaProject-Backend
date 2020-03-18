c181d29be2e07637fbb2ba6ff4a85d0733c7f6df
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-18 19:54:51 +0100

    Add send email functionality to register controller

--- src/main/java/com/DTeam/eshop/controllers/RegisterController.java
+++ src/main/java/com/DTeam/eshop/controllers/RegisterController.java
@@ -9,6 +9,7 @@ import com.DTeam.eshop.entities.Role;
 import com.DTeam.eshop.entities.User;
 import com.DTeam.eshop.services.RoleService;
 import com.DTeam.eshop.services.UserService;
+import com.DTeam.eshop.utilities.EmailSender;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,6 +17,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Controller
 public class RegisterController {
@@ -25,6 +28,12 @@ public class RegisterController {
 
     @Autowired
     private RoleService roleService;
+
+    @Autowired
+    private EmailSender emailSender;
+
+    @Autowired
+    private TemplateEngine templateEngine;
 
     @GetMapping("/register")
     public String register(Model model){
@@ -49,6 +58,15 @@ public class RegisterController {
         roleList.add(role);
         user.setRoles(roleList);
         userService.save(user);
+
+        Context context = new Context();
+        context.setVariable("header", "Dziękujemy za rejestrację!");
+        context.setVariable("title", "Witaj, " + user.getEmail());
+        context.setVariable("description", "Dziękujęmy, że zarejestrowałeś się w naszym sklepie. Mamy nadzieję, że znajdziesz tutaj " +
+        "produkty, których szukasz. Zapraszamy :)");
+        String body = templateEngine.process("template", context);
+        emailSender.sendEmail(user.getEmail(), "Rejestracja udana", body);
+
         model.addAttribute("success", true);
         return "views/register";
     }
