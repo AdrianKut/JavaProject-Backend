2fd6de36c24892296c17769c7d6ff59d92ad082c
MichalStygar <misiek9753@o2.pl>
2020-03-19 19:11:20 +0100

    Create edit function

--- src/main/java/com/DTeam/eshop/controllers/employee/EmployeeOrderController.java
+++ src/main/java/com/DTeam/eshop/controllers/employee/EmployeeOrderController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeOrderController {
@@ -21,6 +23,23 @@ public class EmployeeOrderController {
         List<Order> orderList = orderService.getOrderByStatus();
         model.addAttribute("orderList",orderList);
         return "views/employee/list";
+    }
+
+    @GetMapping("/employee/order/edit/{id}")
+    public String edit(Model model,@PathVariable(name = "id") long id){
+        Order order = orderService.get(id);
+        model.addAttribute("order", order);
+        return "views/employee/edit";
+    }
+
+    @PostMapping("/employee/order/edit/{id}")
+    public String edit(@PathVariable(name = "id") long id,
+    Order order){
+        Order currnetOrder = orderService.get(id);
+        currnetOrder.setShipmentDate(order.getShipmentDate());
+        currnetOrder.setOrderStatus(order.getOrderStatus());
+        orderService.saveEdit(currnetOrder);
+        return "redirect:/employee/order/list";
     }
 
 }
