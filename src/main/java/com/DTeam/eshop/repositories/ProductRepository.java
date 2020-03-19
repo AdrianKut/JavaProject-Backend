dd7130e3b163fbe48ffd9683da8013b0bf8cad49
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-19 15:22:04 +0100

    Add pagination functionality

--- src/main/java/com/DTeam/eshop/repositories/ProductRepository.java
+++ src/main/java/com/DTeam/eshop/repositories/ProductRepository.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import com.DTeam.eshop.entities.Product;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByOrdersOrderId(Long orderId);
+    Page<Product> findByNameContaining(String name, Pageable pageable);
+    List<Product> findByNameContaining(String name);
 }
