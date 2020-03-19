16435751bc13692d385671cc3b4c0f1587c6948b
MichalStygar <misiek9753@o2.pl>
2020-03-19 17:31:53 +0100

    Add new function

--- src/main/java/com/DTeam/eshop/repositories/OrderRepository.java
+++ src/main/java/com/DTeam/eshop/repositories/OrderRepository.java
@@ -10,5 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
 	List<Order> findByCustomer(Customer customer);
-    
+    List<Order> findByOrderStatusNotLike(String ordername);
 }
