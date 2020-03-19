8be382f97a31483fac965a3f62c5d2a2a4f3a80b
MichalStygar <misiek9753@o2.pl>
2020-03-19 16:36:47 +0100

    Create function

--- src/main/java/com/DTeam/eshop/repositories/OrderRepository.java
+++ src/main/java/com/DTeam/eshop/repositories/OrderRepository.java
@@ -1,9 +1,14 @@
 package com.DTeam.eshop.repositories;
 
+import java.util.List;
+
+import com.DTeam.eshop.entities.Customer;
 import com.DTeam.eshop.entities.Order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+	List<Order> findByCustomer(Customer customer);
     
 }
