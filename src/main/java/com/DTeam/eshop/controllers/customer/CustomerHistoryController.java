a6c08a96bca4c8cd982bd8e0122eff714f2e5301
MichalStygar <misiek9753@o2.pl>
2020-03-19 16:34:22 +0100

    Create customer history controller

---
+++ src/main/java/com/DTeam/eshop/controllers/customer/CustomerHistoryController.java
@@ -0,0 +1,48 @@
+CustromerHistoryController
+
+package com.DTeam.eshop.controllers.customer;
+
+import java.security.Principal;
+import java.util.List;
+
+import com.DTeam.eshop.entities.Customer;
+import com.DTeam.eshop.entities.Order;
+import com.DTeam.eshop.services.CustomerService;
+import com.DTeam.eshop.services.OrderService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomerHistoryController {
+   
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private OrderService orderService;
+
+
+
+    
+
+    @GetMapping("/customer/history")
+    public String history(Model model, Principal principal) {
+
+        String email = principal.getName();
+        if (customerService.isCustomerExist(email)) {
+            Customer customer = customerService.getByEmail(email);
+            List<Order> orderList = orderService.getByCustomer(customer);
+            model.addAttribute("orderList", orderList);
+            return "views/customer/history";
+            
+        } else{
+            return "/index";
+        }
+              
+        
+    }
+    
+}
