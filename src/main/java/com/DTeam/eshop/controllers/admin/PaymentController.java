8648610c4525839a18045dc950f7866477e06452
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-20 12:05:38 +0100

    Delete payment from order controller

--- src/main/java/com/DTeam/eshop/controllers/admin/PaymentController.java
+++ src/main/java/com/DTeam/eshop/controllers/admin/PaymentController.java
@@ -2,9 +2,7 @@ package com.DTeam.eshop.controllers.admin;
 
 import java.util.List;
 
-import com.DTeam.eshop.entities.Order;
 import com.DTeam.eshop.entities.Payment;
-import com.DTeam.eshop.services.OrderService;
 import com.DTeam.eshop.services.PaymentService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +11,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PaymentController {
 
     @Autowired
     private PaymentService paymentService;
-
-    @Autowired
-    private OrderService orderService;
 
     @GetMapping("/admin/payment/list")
     public String getAll(Model model){
@@ -33,35 +27,27 @@ public class PaymentController {
 
     @GetMapping("/admin/payment/add")
     public String save(Model model) {
-        List<Order> orderList = orderService.listAll();
         Payment payment = new Payment();
         model.addAttribute("payment", payment);
-        model.addAttribute("orderList", orderList);
         return "views/admin/payment/add";
     }
 
     @PostMapping("/admin/payment/add")
-    public String save(Payment payment,
-        @RequestParam(name="order")Order order){
-        payment.setOrder(order);
+    public String save(Payment payment){
         paymentService.save(payment);
         return "redirect:/admin/payment/list";
     }
 
     @GetMapping("/admin/payment/edit/{id}")
     public String edit(Model model,@PathVariable(name = "id") long id){
-        List<Order> orderList = orderService.listAll();
         Payment payment = paymentService.get(id);
         model.addAttribute("payment", payment);
-        model.addAttribute("orderList", orderList);
         return "views/admin/payment/edit";
     }
 
     @PostMapping("/admin/payment/edit/{id}")
-    public String edit(@PathVariable(name = "id") long id, Payment payment,
-        @RequestParam(name="order")Order order){
+    public String edit(@PathVariable(name = "id") long id, Payment payment){
         payment.setPaymentId(id);
-        payment.setOrder(order);
         paymentService.save(payment);
         return "redirect:/admin/payment/list";
     }
@@ -71,6 +57,4 @@ public class PaymentController {
         paymentService.delete(id);
         return "redirect:/admin/payment/list";
     }
-
-
 }
