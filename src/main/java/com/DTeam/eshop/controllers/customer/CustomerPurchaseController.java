2416c600cd07b36f16c79c0cd0c2bbf0b1e1f1d4
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-20 19:33:04 +0100

    Add functionality to purchase a single product

--- src/main/java/com/DTeam/eshop/controllers/customer/CustomerPurchaseController.java
+++ src/main/java/com/DTeam/eshop/controllers/customer/CustomerPurchaseController.java
@@ -2,6 +2,7 @@ package com.DTeam.eshop.controllers.customer;
 
 import java.security.Principal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.DTeam.eshop.entities.Customer;
@@ -20,8 +21,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Controller
 public class CustomerPurchaseController {
@@ -37,9 +40,6 @@ public class CustomerPurchaseController {
 
     @Autowired
     private CustomerService customerService;
-
-    @Autowired
-    private AddressService addressService;
 
     @Autowired
     private ShoppingCart shoppingCart;
@@ -85,6 +85,61 @@ public class CustomerPurchaseController {
         orderService.save(order, productList);
         shoppingCart.clear();
 
+        Context context = new Context();
+        context.setVariable("header", "Dziękujemy za zakup!");
+        context.setVariable("title", "Witaj, " + email);
+        context.setVariable("description", "Dziękujęmy, że zakupiłeś produkty w naszym sklepie. Postaramy się, aby jak " +
+        "najszybciej do Ciebie dotarły. Pozdrawiamy :)");
+        String body = templateEngine.process("template", context);
+        emailSender.sendEmail(email, "Zakup produktów", body);
+
         return "redirect:/shopping-cart";
+    }
+
+    @GetMapping("/customer/buy/{id}")
+    public String buyProduct(Model model, Principal principal,
+     @PathVariable(name = "id")Long id){
+        String email = principal.getName();
+        if (customerService.isCustomerExist(email)) {
+            Payment payment = new Payment();
+            Product product = productService.get(id);
+            model.addAttribute("payment", payment);
+            model.addAttribute("amount", product.getPrice());
+            return "views/customer/buy";
+        } else {
+            return "redirect:/";
+        }
+    }
+
+    @PostMapping("/customer/buy/{id}")
+    public String buyProduct(Payment payment, Principal principal,
+    @PathVariable(name = "id")Long id){
+        Product product = productService.get(id);
+        product.setQuantity(product.getQuantity()-1);
+        productService.save(product);
+        String email = principal.getName();
+        LocalDateTime dateTime = LocalDateTime.now();
+        payment.setPaymentDate(dateTime.toString());
+        payment.setAmount(product.getPrice());
+        paymentService.save(payment);
+        Order order = new Order();
+        Customer customer = customerService.getByEmail(email);
+        order.setCustomer(customer);
+        order.setPurchaseDate(dateTime.toString());
+        order.setOrderStatus("Przyjęte");
+        order.setPayment(payment);
+        List<Product> productList = new ArrayList<Product>();
+        productList.add(product);
+        orderService.save(order, productList);
+
+        Context context = new Context();
+        context.setVariable("header", "Dziękujemy za zakup!");
+        context.setVariable("title", "Witaj, " + email);
+        context.setVariable("description", "Dziękujęmy, że zakupiłeś produkty w naszym sklepie. Postaramy się, aby jak " +
+        "najszybciej do Ciebie dotarły. Pozdrawiamy :)");
+        String body = templateEngine.process("template", context);
+        emailSender.sendEmail(email, "Zakup produktów", body);
+
+        return "redirect:/customer/order";
     }
 }
