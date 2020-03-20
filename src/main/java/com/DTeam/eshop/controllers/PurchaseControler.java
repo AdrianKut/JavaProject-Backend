ae4cde9ebd14c1025caf6db3c7525bd41f46f353
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-20 13:12:20 +0100

    Add purchase functionality without registration

--- src/main/java/com/DTeam/eshop/controllers/PurchaseControler.java
+++ src/main/java/com/DTeam/eshop/controllers/PurchaseControler.java
@@ -14,6 +14,7 @@ import com.DTeam.eshop.services.OrderService;
 import com.DTeam.eshop.services.PaymentService;
 import com.DTeam.eshop.services.ProductService;
 import com.DTeam.eshop.utilities.ShoppingCart;
+import com.DTeam.eshop.utilities.EmailSender;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,6 +22,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Controller
 public class PurchaseControler {
@@ -43,6 +46,12 @@ public class PurchaseControler {
     @Autowired
     private ShoppingCart shoppingCart;
 
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Autowired
+    private EmailSender emailSender;
+
     @GetMapping("/shopping-cart/buy/no-register")
     public String ShoppingCartNoRegister(Model model){
         Address address = new Address();
@@ -62,14 +71,31 @@ public class PurchaseControler {
         addressService.save(address);
         customer.setAddress(address);
         customerService.save(customer);
+        LocalDateTime dateTime = LocalDateTime.now();
+        payment.setPaymentDate(dateTime.toString());
+        payment.setAmount(shoppingCart.getAmount());
+        paymentService.save(payment);
         Order order = new Order();
         order.setCustomer(customer);
         order.setOrderStatus("Przyjęte");
-        LocalDateTime dateTime = LocalDateTime.now();
         order.setPurchaseDate(dateTime.toString());
+        order.setPayment(payment);
         List<Product> productList = shoppingCart.get();
+        for (Product product : productList) {
+            product.setQuantity(product.getQuantity()-1);
+            productService.save(product);
+        }
         orderService.save(order, productList);
-        //payment
-        return "/";
+        shoppingCart.clear();
+
+        Context context = new Context();
+        context.setVariable("header", "Dziękujemy za zakup!");
+        context.setVariable("title", "Witaj, " + email);
+        context.setVariable("description", "Dziękujęmy, że zakupiłeś produkty w naszym sklepie. Postaramy się, aby jak " +
+        "najszybciej do Ciebie dotarły. Pozdrawiamy :)");
+        String body = templateEngine.process("template", context);
+        emailSender.sendEmail(email, "Zakup produktów", body);
+
+        return "redirect:/shopping-cart";
     }
 }
