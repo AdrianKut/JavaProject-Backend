83f8dfa56ebbcffc5f514cf5ed6baa6606cb248c
adrin96 <adrin99@o2.pl>
2020-03-21 13:46:25 +0100

    Unit test listAll() PaymentServiceTest

---
+++ src/test/java/com/DTeam/eshop/services/PaymentServiceTest.java
@@ -0,0 +1,61 @@
+package com.DTeam.eshop.services;
+
+import com.DTeam.eshop.entities.Payment;
+import java.time.LocalDateTime;
+
+import java.util.List;
+import java.util.ArrayList;
+import javax.security.auth.message.callback.PrivateKeyCallback;
+import org.junit.jupiter.api.Test;
+
+import org.hamcrest.Matchers;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.junit.Assert.*;
+import org.junit.ComparisonFailure;
+
+public class PaymentServiceTest {
+
+    final PaymentService paymentService = mock(PaymentService.class);
+
+    @Test
+    public void testListAll() {
+
+        when(paymentService.listAll()).thenReturn(prepareMocData());
+
+        assertEquals(paymentService.listAll().get(0).getPaymentId(), null);
+        assertEquals(paymentService.listAll().get(2).getPaymentMethod(), "Gotowka");
+        assertThat(paymentService.listAll(), Matchers.hasSize(3));
+    }
+
+    private List<Payment> prepareMocData() {
+
+        List<Payment> payment = new ArrayList<>();
+        payment.add(new Payment());
+        payment.add(new Payment());
+        payment.add(new Payment(520L, LocalDateTime.MIN, "Gotowka"));
+        return payment;
+    }
+
+    @Test
+    public void testSave() {
+    }
+
+    @Test
+    public void testGet() {
+    }
+
+    @Test
+    public void testDelete() {
+    }
+
+    @Test
+    public void testIsPaymentExist() {
+    }
+
+}
