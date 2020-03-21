f8e1ed732d1baab30003ea5d883391dcc713fcb9
adrin96 <adrin99@o2.pl>
2020-03-21 16:56:03 +0100

    Create RoleServiceTest

---
+++ src/test/java/com/DTeam/eshop/services/RoleServiceTest.java
@@ -0,0 +1,37 @@
+
+package com.DTeam.eshop.services;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class RoleServiceTest {
+    
+    public RoleServiceTest() {
+    }
+
+    @Test
+    public void testListAll() {
+    }
+
+    @Test
+    public void testSave() {
+    }
+
+    @Test
+    public void testGet() {
+    }
+
+    @Test
+    public void testDelete() {
+    }
+
+    @Test
+    public void testIsRoleExist() {
+    }
+
+    @Test
+    public void testGetByUserEmail() {
+    }
+    
+}
