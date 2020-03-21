096804345dafa75fee5806a42ef4467a37b49c4c
adrin96 <adrin99@o2.pl>
2020-03-21 16:55:28 +0100

    Create ProductServiceTest

---
+++ src/test/java/com/DTeam/eshop/services/ProductServiceTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.DTeam.eshop.services;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author AiXen
+ */
+public class ProductServiceTest {
+    
+    public ProductServiceTest() {
+    }
+
+    @Test
+    public void testListAll() {
+    }
+
+    @Test
+    public void testSave() {
+    }
+
+    @Test
+    public void testGet() {
+    }
+
+    @Test
+    public void testDelete() {
+    }
+
+    @Test
+    public void testIsProductExist() {
+    }
+
+    @Test
+    public void testGetByOrderId() {
+    }
+
+    @Test
+    public void testGetByNameOrCategory() {
+    }
+
+    @Test
+    public void testGetByCategory() {
+    }
+    
+}
