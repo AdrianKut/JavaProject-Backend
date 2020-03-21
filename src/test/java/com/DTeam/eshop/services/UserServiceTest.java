28759cede48015a207d1dfb70c616013a39b2e5e
adrin96 <adrin99@o2.pl>
2020-03-21 16:56:23 +0100

    Create UserServiceTest

---
+++ src/test/java/com/DTeam/eshop/services/UserServiceTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.DTeam.eshop.services;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author AiXen
+ */
+public class UserServiceTest {
+    
+    public UserServiceTest() {
+    }
+
+    @Test
+    public void testListAll() {
+    }
+
+    @Test
+    public void testSave() {
+    }
+
+    @Test
+    public void testGet() {
+    }
+
+    @Test
+    public void testDelete() {
+    }
+
+    @Test
+    public void testIsUserExist() {
+    }
+    
+}
